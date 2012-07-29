4b0906400967385eb0af3cff36ad66cee48de0ef
inazaruk <ievgenii.nazaruk@gmail.com>
2012-07-29 17:36:25 +0300

    Fixed unit test which tests unique violations enabler.

--- src/strictmode-lib-test/src/com/robomorphine/strictmode/StrictModeHelperTest.java
+++ src/strictmode-lib-test/src/com/robomorphine/strictmode/StrictModeHelperTest.java
@@ -12,7 +12,7 @@ public class StrictModeHelperTest extends TestCase {
             StrictModeHelper.enableUniqueViolations(true);        
             StrictModeHelper.enableUniqueViolations(false);
             
-            assertFalse(Build.VERSION.SDK_INT > 8);//should not reach this line on 8th or earlier
+            assertTrue(Build.VERSION.SDK_INT > 8);//should not reach this line on 8th or earlier
         } catch (PlatformNotSupportedException ex) {
             assertTrue(Build.VERSION.SDK_INT <= 8);
         }
